f2fad99f33dfe533e48cc692e069968077fc5193
Riccardo Moncada <rmondev@gmail.com>
2025-04-16 12:26:25 -0400

    Fetch and display artwork data in ArtworkCard component

--- src/components/ArtworkCard.jsx
+++ src/components/ArtworkCard.jsx
@@ -1,14 +1,17 @@
+'use client'
 import React, {useEffect, useState} from 'react'
+import Image from 'next/image'
 
 const ArtworkCard = ({objectId}) => {
 
-
+    const [cardData, setCardData] = useState(null)
     useEffect(() => {
         fetchObjectData = async () => {
             try {
                 let response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
                 let data = response.json
                 console.log(data)
+                setCardData(data)
             } catch (error) {
                 console.log(error)
             }
@@ -16,9 +19,46 @@ const ArtworkCard = ({objectId}) => {
     }, [])
 
   return (
-    <div>
-      
-    </div>
+    <>
+    {cardData && (
+        // Card Container
+        <div className='flex flex-col border-2 rounded border-red-600 justify-center items-center gap-2'>
+            {/* Image Container */}
+            <section className='flex justify-center items-center'>
+                {data?.primaryImageSmall ? 
+                <Image
+                alt='artwork-image'
+                src={data?.primaryImageSmall}
+                width={'18rem'}
+                />
+                :
+                <Image
+                alt='image-not-available'
+                src='https://placehold.co/200x300?text=Not+Available&font=roboto'
+                width={'18rem'}
+                />
+                }
+            </section>
+
+            {/* Card Body Container */}
+            <section>
+                {/* Card Title */}
+                {data?.title ?
+                    <h2>{data?.title}</h2>
+                    :
+                    <h2>N/A</h2>
+                }
+                {/* Card Text Container */}
+                <div>
+                    {data?.objectDate ? <p><strong>Date: </strong>{data?.objectDate}</p> : <p><strong>Data: </strong>N/A</p>}
+                    {data?.classification ? <p><strong>Classification: </strong>{data?.classification}</p> : <p><strong>Classification: </strong>N/A</p>}
+                    {data?.medium ? <p><strong>Classification: </strong>{data?.medium}</p> : <p><strong>Medium: </strong>N/A</p>}
+                </div>
+
+            </section>
+        </div>
+    )}
+    </>
   )
 }
 
